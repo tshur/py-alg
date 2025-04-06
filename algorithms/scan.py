ce14748cacb82430ee6bd67fec6223e13bfe8bdf
Tim Shur <tim.shur@gmail.com>
2025-04-06 17:47:58 -0400

    Add generic typing for scan.

--- algorithms/scan.py
+++ algorithms/scan.py
@@ -1,29 +1,29 @@
 import operator
-from typing import Any, Callable, Iterable, Iterator, Optional
+from typing import Callable, Iterable, Iterator, Optional
 
 
-def scan(
-    iterable: Iterable[Any],
-    function: Callable[[Any, Any], Any] = operator.add,
+def scan[T](
+    iterable: Iterable[T],
+    function: Callable[[T, T], T] = operator.add,
     *,
-    initial: Optional[Any] = None,
-) -> Iterator[Any]:
+    initial: Optional[T] = None,
+) -> Iterator[T]:
     """Generate accumulated values from an iterable (left scan algorithm).
 
     Optionally takes an accumulator function (in the format of reduce(acc, val)). Can
     take an initial value, which will be yielded first.
 
     Args:
-        iterable (Iterable[Any]): Input values to accumulate. Can be empty.
-        function (Callable[[Any, Any], Any], optional): Function to call on each value.
+        iterable (Iterable[T]): Input values to accumulate. Can be empty.
+        function (Callable[[T, T], T], optional): Function to call on each value.
           The first argument will be the accumulator, and the second argument will be
           the new value from the iterable. Defaults to operator.add.
-        initial (Optional[Any], optional): An initial value for the accumulator. If
+        initial (Optional[T], optional): An initial value for the accumulator. If
           given, the output will start with this value and be one longer than the input
           iterable. Defaults to None.
 
     Yields:
-        Iterator[Any]: Generated values. If initial is given, the first value generated
+        Iterator[T]: Generated values. If initial is given, the first value generated
           will be initial and the output will be one longer than the input.
 
     Examples:
@@ -37,8 +37,9 @@ def scan(
         []
     """
     iterator = iter(iterable)
-    total = initial
-    if initial is None:
+    if initial is not None:
+        total = initial
+    else:
         try:
             total = next(iterator)
         except StopIteration:
