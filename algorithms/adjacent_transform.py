c4c32ec11c059bdda56dd0473c1a154abbe939b1
Tim Shur <tim.shur@gmail.com>
2025-04-07 18:20:18 -0400

    Add generalized length adjacent_transform.

--- algorithms/adjacent_transform.py
+++ algorithms/adjacent_transform.py
@@ -1,6 +1,56 @@
 import operator
 from itertools import tee
-from typing import Callable, Iterable, Iterator
+from typing import Callable, Iterable, Iterator, Tuple
+
+
+def adjacent_transform[T, U](
+    iterable: Iterable[T],
+    function: Callable[[Tuple[T, ...]], U],
+    window_size: int = 2,
+) -> Iterator[U]:
+    """Applies a function on a sliding window from the input iterable.
+
+    Similar / equivalent algorithms: itertools.pairwise, pairwise_transform, sliding
+    window.
+
+    Args:
+        iterable (Iterable[T]): Input values, which will be traversed in windows of size
+          window_size.
+        function (Callable[[Tuple[T, ...]], U]): Function to call on each window of
+          values. The function will be called with a tuple of length matching the
+          window_size.
+        window_size (int, optional): Window size to apply the function. This value
+          represents the number of adjacent elements to give to the function. Defaults
+          to 2 (pairwise_transform).
+
+    Yields:
+        Iterator[U]: Generated values. The length of the output iterator will be
+        len(iterable) - (window_size - 1).
+
+    Raises:
+        ValueError: If window_size < 0.
+
+    Examples:
+        >>> list(adjacent_transform([15, 10, 6, 3, 1], sum))
+        [25, 16, 9, 4]
+        >>> list(adjacent_transform([15, 10, 6, 3, 1], sum, 3))
+        [31, 19, 10]
+        >>> list(adjacent_transform([10, 10], sum, 1))
+        [10, 10]
+        >>> list(adjacent_transform([10, 10], sum, 3))
+        []
+    """
+    iterators = tee(iterable, window_size)
+    try:
+        # Offset the iterators according to their index to create the window.
+        for i, iterator in enumerate(iterators):
+            for _ in range(i):
+                next(iterator)
+    except StopIteration:
+        return  # Not enough elements to access pairwise.
+
+    for window in zip(*iterators):
+        yield function(window)
 
 
 def pairwise_transform[T, U](
@@ -18,8 +68,8 @@ def pairwise_transform[T, U](
           (i.e., (iterable[i], iterable[i+1])). Defaults to operator.add.
 
     Yields:
-        Iterator[U]: Generated values. The length of the output iterate will be one less
-        than the input iterator.
+        Iterator[U]: Generated values. The length of the output iterator will be one
+        less than the input iterator.
 
     Examples:
         >>> list(pairwise_transform([15, 10, 6, 3, 1]))
@@ -29,11 +79,9 @@ def pairwise_transform[T, U](
         >>> list(pairwise_transform([10]))
         []
     """
-    first_iterator, second_iterator = tee(iterable, 2)
-    try:
-        next(second_iterator)
-    except StopIteration:
-        return  # Not enough elements to access pairwise.
 
-    for first, second in zip(first_iterator, second_iterator):
-        yield function(first, second)
+    # We need to adjust the argument shape to use the generic adjacent_transform.
+    def tuple_function(values: tuple[T, ...]) -> U:
+        return function(*values)
+
+    yield from adjacent_transform(iterable, tuple_function, 2)
--- algorithms/adjacent_transform_test.py
+++ algorithms/adjacent_transform_test.py
@@ -1,6 +1,34 @@
 import operator
 
-from algorithms.adjacent_transform import pairwise_transform
+from algorithms.adjacent_transform import adjacent_transform, pairwise_transform
+
+
+class TestAdjacentTransform:
+    def test_pairwise_transform_sum(self):
+        assert list(adjacent_transform([1, 2, 3, 4, 5], sum)) == [3, 5, 7, 9]
+        assert list(adjacent_transform([-5, 5], sum)) == [0]
+        assert list(adjacent_transform([1, 1, 1, 1, 1], sum)) == [2, 2, 2, 2]
+
+    def test_adjacent_transform_sum(self):
+        assert list(adjacent_transform([1, 2, 3, 4, 5], sum, 3)) == [6, 9, 12]
+        assert list(adjacent_transform([1, 2, 3, 4, 5], sum, 4)) == [10, 14]
+        assert list(adjacent_transform([1, 2, 3, 4, 5], sum, 5)) == [15]
+
+    def test_input_too_short(self):
+        assert list(adjacent_transform([], sum)) == []
+        assert list(adjacent_transform([5], sum)) == []
+        assert list(adjacent_transform([-5, 5], sum, 3)) == []
+
+    def test_small_window_size(self):
+        assert list(adjacent_transform([1, 2, 3], sum, 1)) == [1, 2, 3]
+        assert list(adjacent_transform([1, 2, 3], sum, 0)) == []
+
+    def test_moving_average(self):
+        def average(nums: tuple[int, ...]) -> float:
+            return sum(nums) / len(nums)
+
+        assert list(adjacent_transform([1, 3, -1, 1], average)) == [2, 1, 0]
+        assert list(adjacent_transform([1, 2, 5], average)) == [1.5, 3.5]
 
 
 class TestPairwiseTransform:
