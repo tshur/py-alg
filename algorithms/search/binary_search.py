13c37dce4cad9f197d58658ed4443a45bbc5b44e
Tim Shur <tim.shur@gmail.com>
2025-04-08 23:35:09 -0400

    Add lower_bound and upper_bound algorithms.

--- algorithms/search/binary_search.py
+++ algorithms/search/binary_search.py
@@ -29,18 +29,103 @@ def binary_search[CT: Comparable](array: Sequence[CT], target: CT) -> Optional[i
           target has multiple occurrences, one index will be returned. If the target is
           not found, returns None.
     """
-    # Define the search space as [0, len(array)), with an exclusive end bounds to match
-    # how slicing does it.
-    lo = 0
-    hi = len(array)
+    # Search space is [0, len(array)), with an exclusive end to match slicing.
+    low = 0
+    high = len(array)
 
-    while lo < hi:
-        mid = (lo + hi) // 2
+    while low < high:
+        mid = (low + high) // 2
         if target == array[mid]:
             return mid
 
         if array[mid] < target:
-            lo = mid + 1
+            low = mid + 1
         else:
-            hi = mid
+            high = mid
     return None
+
+
+def lower_bound[CT: Comparable](array: Sequence[CT], target: CT) -> int:
+    """Uses binary search to find the smallest index where target could be inserted.
+
+    This function has the difference with the core binary_search function in two ways.
+    First, if the value is not found in the array, lower_bound will not return None, but
+    an index where the target could be inserted while maintaining sorted order. Second,
+    if there are multiple copies of the target, this function will return the leftmost
+    index.
+
+    Sample: (L=low, M=mid, H=high)
+    [1, 3, 3, 3, 5, 6, 7], target=3
+     L        M           H (mid is not too low)
+     L  M     H             (mid is not too low)
+     LM H                   (mid is too low)
+       LMH                  (break loop; return L index)
+
+    Complexity:
+        Time: O(logn)
+        Space: O(1)
+
+    Args:
+        array (Sequence[CT]): The input sequence to be searched. This must be in
+          (ascending) sorted order.
+        target (CT): The target value to find.
+
+    Returns:
+        int: The lowest index where the target could be inserted into the input sequence
+          while maintaining sorted order. If the target is present in the array, this is
+          equal to the leftmost index of the target.
+    """
+    # Search space is [0, len(array)), with an exclusive end to match slicing.
+    low = 0
+    high = len(array)
+
+    while low < high:
+        mid = (low + high) // 2
+        if array[mid] < target:
+            low = mid + 1
+        else:
+            high = mid
+    return low
+
+
+def upper_bound[CT: Comparable](array: Sequence[CT], target: CT) -> int:
+    """Uses binary search to find the largest index where target could be inserted.
+
+    This returns the first index in the array with a value greater than the target. If
+    no such index exists, returns the length of the array. This is the largest index
+    where the target could be inserted into the array while still maintaining sorted
+    order.
+
+    Sample: (L=low, M=mid, H=high)
+    [1, 3, 3, 3, 5, 6, 7], target=3
+     L        M           H (mid is not too high)
+              L     M     H (mid is too high)
+              L  M  H       (mid is too high)
+              LM H          (mid is not too high)
+                LMH         (break loop; return L index)
+
+    Complexity:
+        Time: O(logn)
+        Space: O(1)
+
+    Args:
+        array (Sequence[CT]): The input sequence to be searched. This must be in
+          (ascending) sorted order.
+        target (CT): The target value to find.
+
+    Returns:
+        int: The highest index where the target could be inserted into the input
+          sequence while maintaining sorted order. If the target is present in the
+          array, this is equal to one after the rightmost index of the target.
+    """
+    # Search space is [0, len(array)), with an exclusive end to match slicing.
+    low = 0
+    high = len(array)
+
+    while low < high:
+        mid = (low + high) // 2
+        if target < array[mid]:
+            high = mid
+        else:
+            low = mid + 1
+    return low
