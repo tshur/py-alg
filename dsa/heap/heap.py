acc73b6b45249214063f7cad9e74415820a99151
Tim Shur <tim.shur@gmail.com>
2025-04-18 18:09:09 -0400

    Write working heap implementation.

--- dsa/heap/heap.py
+++ dsa/heap/heap.py
@@ -1,27 +1,112 @@
-from typing import Iterable, Optional, Self
+from typing import Iterable, Iterator, Optional, Self
 
 from dsa.typing.comparison import Comparable
 
 
 class Heap[CT: Comparable]:
+    _heap: list[CT]
+
+    class _Node[T]:
+        index: int
+        value: T
+
+        def __init__(self, index: int, value: T):
+            self.index = index
+            self.value = value
+
     def __init__(self):
-        raise NotImplementedError
+        self._heap = []
 
     @classmethod
     def from_iterable(cls, iterable: Iterable[CT]) -> Self:
-        raise NotImplementedError
+        heap = cls()
+        heap._heap = list(iterable)
+        heap._heapify()
+        return heap
 
     def push(self, value: CT) -> None:
-        raise NotImplementedError
+        self._heap.append(value)
+        self._sift_up(len(self) - 1)
 
     def pop(self) -> Optional[CT]:
-        raise NotImplementedError
+        if self.is_empty():
+            return None
+        if len(self) == 1:
+            return self._heap.pop()
+
+        removed = self._heap[0]
+        self._heap[0] = self._heap.pop()
+        print(self)
+        self._sift_down(0)
+        print(self)
+        print()
+        return removed
 
     def peek(self) -> Optional[CT]:
-        raise NotImplementedError
+        if self.is_empty():
+            return None
+        return self._heap[0]
 
     def is_empty(self) -> bool:
-        raise NotImplementedError
+        return len(self) == 0
+
+    def consume_all(self) -> Iterator[CT]:
+        while True:
+            value = self.pop()
+            if value is None:
+                break
+            yield value
 
     def __len__(self) -> int:
-        raise NotImplementedError
+        return len(self._heap)
+
+    def _parent(self, index: int) -> Optional[_Node[CT]]:
+        parent_index = (index - 1) // 2
+        if parent_index < 0:
+            return None
+        return Heap._Node(parent_index, self._heap[parent_index])
+
+    def _left_child(self, index: int) -> Optional[_Node[CT]]:
+        child_index = index * 2 + 1
+        if child_index >= len(self):
+            return None
+        return Heap._Node(child_index, self._heap[child_index])
+
+    def _right_child(self, index: int) -> Optional[_Node[CT]]:
+        child_index = index * 2 + 2
+        if child_index >= len(self):
+            return None
+        return Heap._Node(child_index, self._heap[child_index])
+
+    def _smaller_child(self, index: int) -> Optional[_Node[CT]]:
+        left, right = self._left_child(index), self._right_child(index)
+        if not left or not right:
+            return left
+
+        if left.value < right.value:
+            return left
+        return right
+
+    def _sift_up(self, index: int) -> None:
+        while index >= 0:
+            current = self._heap[index]
+            parent = self._parent(index)
+            if not parent or parent.value < current:
+                return  # Satisfies heap ordering.
+
+            self._heap[parent.index], self._heap[index] = current, parent.value
+            index = parent.index
+
+    def _sift_down(self, index: int) -> None:
+        while index < len(self):
+            current = self._heap[index]
+            child = self._smaller_child(index)
+            if not child or current < child.value:
+                return  # Satisfies heap ordering.
+
+            self._heap[index], self._heap[child.index] = child.value, current
+            index = child.index
+
+    def _heapify(self) -> None:
+        for i in range(len(self) - 1, -1, -1):
+            self._sift_down(i)
--- dsa/heap/heap_test.py
+++ dsa/heap/heap_test.py
@@ -9,11 +9,9 @@ class TestHeap:
         assert len(heap) == 0
 
     def test_from_iterable(self) -> None:
-        heap = Heap[int].from_iterable([7, 5, 1, 3, 9, 0, 4, 2, 8])
+        heap = Heap[int].from_iterable([7, 5, 1, 3, 9, 0, 4, 6, 2, 8])
 
-        heap_sorted = [heap.pop() for _ in range(len(heap))]
-
-        assert heap_sorted == list(range(10))
+        assert list(heap.consume_all()) == list(range(10))
 
     def test_push(self) -> None:
         heap = Heap[int]()
@@ -22,12 +20,16 @@ class TestHeap:
         heap.push(4)
         heap.push(6)
         heap.push(8)
+        assert heap.peek() == 4
         assert len(heap) == 3
 
         # Push at head.
         heap.push(2)
+        assert heap.peek() == 2
         heap.push(1)
+        assert heap.peek() == 1
         heap.push(0)
+        assert heap.peek() == 0
         assert len(heap) == 6
 
         # Push middle nodes.
@@ -35,40 +37,49 @@ class TestHeap:
         heap.push(7)
         heap.push(9)
         heap.push(3)
+        assert heap.peek() == 0
         assert len(heap) == 10
 
         # Push repeated elements.
         heap.push(3)
         heap.push(3)
         heap.push(3)
+        assert heap.peek() == 0
         assert len(heap) == 13
 
         # Verify pop order.
-        heap_sorted = [heap.pop() for _ in range(len(heap))]
-        assert heap_sorted == [0, 1, 2, 3, 3, 3, 3, 4, 5, 6, 7, 8, 9]
+        assert list(heap.consume_all()) == [0, 1, 2, 3, 3, 3, 3, 4, 5, 6, 7, 8, 9]
 
     def test_pop(self) -> None:
         heap = Heap[int].from_iterable([0, 4, 5, 1, -2, 12, -1, 9, -1, -1])
 
-        heap_sorted = [heap.pop() for _ in range(len(heap))]
-        assert heap_sorted == [-2, -1, -1, -1, 0, 1, 4, 5, 9, 12]
+        assert list(heap.consume_all()) == [-2, -1, -1, -1, 0, 1, 4, 5, 9, 12]
 
     def test_peek(self) -> None:
         assert Heap[int].from_iterable([5, 1, 3, 2, 4]).peek() == 1
         assert Heap[int].from_iterable([5]).peek() == 5
         assert Heap[int].from_iterable([0, 4, 5, 1, -2, 12, -1, 9]).peek() == -2
 
-    def test_pop_peek_on_empty(self) -> None:
+    def test_remove_on_empty(self) -> None:
         heap = Heap[int]()
 
         assert heap.peek() is None
         assert heap.pop() is None
+        assert list(heap.consume_all()) == []
 
     def test_full_push_pop(self) -> None:
         heap = Heap[int]()
 
-        for value in [7, 5, 1, 3, 9, 0, 4, 2, 8]:
+        for value in [7, 5, 1, 3, 9, 0, 4, 6, 2, 8]:
             heap.push(value)
-        heap_sorted = [heap.pop() for _ in range(len(heap))]
 
-        assert heap_sorted == list(range(10))
+        assert list(heap.consume_all()) == list(range(10))
+
+    def test_large_heap(self) -> None:
+        heap1 = Heap[int].from_iterable(range(100))
+        assert list(heap1.consume_all()) == list(range(100))
+
+        heap2 = Heap[int]()
+        for value in range(100):
+            heap2.push(value)
+        assert list(heap2.consume_all()) == list(range(100))
